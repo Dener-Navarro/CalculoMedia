f82d0373576808d9a836e02ea70d1897d409bad1
Dener Navarro <dener.navarro@gmail.com>
2023-08-28 12:46:31 -0300

    commit inicial

---
+++ src/CalculoMedia.java
@@ -0,0 +1,12 @@
+public class CalculoMedia {
+    public static void main(String[] args) {
+        double Atividade1 = 10;
+        double Atividade2 = 20;
+        double Atividade3 = 5.5;
+
+        double media = (Atividade1 + Atividade2 + Atividade3) / 3;
+
+        System.out.println("A média é: " + media);
+    }
+}
+
